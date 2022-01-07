9658be8fc501045575316014ae57c4cffb5a76cd
RanmaOVA <Masseymichael643@gmail.com>
2022-01-07 09:14:56 -0600

    added repositories and dtos

---
+++ Extensions.cs
@@ -0,0 +1,20 @@
+using Catalog.Dtos;
+using Catalog.Repositories;
+
+namespace Catalog
+{
+    public static class Extensions
+    {
+        public static ItemDto AsDto(this Item item)
+        {
+            return new ItemDto
+            {
+                Id = item.Id,
+                Name = item.Name,
+                Price = item.Price,
+                CreatedDate = item.CreatedDate
+            };
+
+        }
+    }
+}
---
+++ Repositories/IItemsRepository.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+
+namespace Catalog.Repositories
+{
+    public interface IItemsRepository
+    {
+        Item GetItem(Guid id);
+        IEnumerable<Item> GetItems();
+    }
+}
